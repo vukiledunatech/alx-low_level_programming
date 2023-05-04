5e623b5d910c362b4a450fe38483959322f13a5f
vukiledunatech <sanezisoduna2@gmail.com>
2023-05-04 12:27:44 -0700

    DUVA Dunique

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,17 +1,22 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-*struct listint_s - singly linked list
-*@n: integer
-*@next: points to the next node
-*
-*Description: singly linked list node structure
-*
-*/
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for alx project
+ */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
-}listint_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -28,4 +33,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif
