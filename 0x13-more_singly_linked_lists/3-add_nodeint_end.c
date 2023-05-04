67f9aaef5913c2e7a10fa2908bb73221ad411838
vukiledunatech <sanezisoduna2@gmail.com>
2023-05-04 13:39:53 -0700

    DUVA Dunique

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,22 +1,34 @@
 #include "lists.h"
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-listint_t *head;
 
-head = NULL;
-add_nodeint_end(&head, 0);
-add_nodeint_end(&head, 1);
-add_nodeint_end(&head, 2);
-add_nodeint_end(&head, 3);
-add_nodeint_end(&head, 4);
-add_nodeint_end(&head, 98);
-add_nodeint_end(&head, 402);
-add_nodeint_end(&head, 1024);
-print_listint(head);
-return (0);
+/**
+ * add_nodeint_end - adds a node at the end of a linked list
+ * @head: pointer to the first element in the list
+ * @n: data to insert in the new element
+ *
+ * Return: pointer to the new node, or NULL if it fails
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *temp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
 }
